8a3757452a5d3804c903146717acbef32b3863a7
ryazlee <ryan.j.lee99@gmail.com>
2017-11-16 13:30:26 -0800

    able to get the html code from websitegit status (for dailystickerart website...)

--- java/lyricFileCreator.java
+++ java/lyricFileCreator.java
@@ -1,6 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+
 public class lyricFileCreator{
     public static void main (String[] args){
-        String baseSite = "http://www.mldb.org/song-244405-y.html"
-    
+        String baseSite = "https://genius.com/#FILLIN#-lyrics";
+        String input = "taylor-swift-you-belong-with-me";
+
+        String url = searchAndReplace("#FILLIN#", input, baseSite);
+        System.out.println(url);        
+        try{        
+            URL site = new URL(url);        
+            
+            System.setProperty("http.agent", "Chrome"); 
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(site.openStream()));    
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) System.out.println(inputLine);
+            in.close();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public static String searchAndReplace(String searchParam, String replacedParam, String base) {
+        String newText = base.replaceAll(searchParam, replacedParam);
+        return newText;
     }
 }
