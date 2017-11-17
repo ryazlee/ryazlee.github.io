10ab2e95cd5c27acbd37f30f518b039a381debcd
ryazlee <ryan.j.lee99@gmail.com>
2017-11-17 15:56:06 -0800

    changed the html formatting a little

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,1 +1,20 @@
-document.getElementById("demo").innerHTML = "Hello Dolly.";
+
+    div {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+        position: relative;
+        animation-name: example;
+        animation-duration: 4s;
+        animation-iteration-count: infinite;
+    }
+
+/* Standard syntax */
+    @keyframes example {
+        0%   {background-color:red; left:0px; top:0px;}
+        25%  {background-color:yellow; left:95%; top:0px;}
+        50%  {background-color:blue; left:95%; top:300px;}
+        75%  {background-color:green; left:0px; top:300px;}
+        100% {background-color:red; left:0px; top:0px;}
+    }
+ 
