b9f183447d67a6b3ee48983cba2eb0d01ed75b98
ryazlee <ryan.j.lee99@gmail.com>
2017-11-16 20:04:42 -0800

    worked on java code

--- java/lyricFileCreator.java
+++ java/lyricFileCreator.java
@@ -4,11 +4,14 @@ import java.io.*;
 
 public class lyricFileCreator{
     public static void main (String[] args){
-        String baseSite = "https://genius.com/#FILLIN#-lyrics";
-        String input = "taylor-swift-you-belong-with-me";
+        String baseSite = "https://www.azlyrics.com/lyrics/#FILLIN#.html";
+        String input = "taylorswift/youbelongwithme";
 
         String url = searchAndReplace("#FILLIN#", input, baseSite);
         System.out.println(url);        
+        
+        String data = "";
+        
         try{        
             URL site = new URL(url);        
             
@@ -17,11 +20,17 @@ public class lyricFileCreator{
             BufferedReader in = new BufferedReader(new InputStreamReader(site.openStream()));    
 
             String inputLine;
-            while ((inputLine = in.readLine()) != null) System.out.println(inputLine);
+            while ((inputLine = in.readLine()) != null){
+                if (inputLine.equals("      ")) data += "\n";
+                else data += inputLine;
+            }
             in.close();
         }catch (IOException e){
             System.out.println(e);
         }
+
+            data = data.substring(data.indexOf("<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->")+133, data.indexOf("<!-- MxM banner -->") - 14);
+            System.out.println(data);
     }
 
     public static String searchAndReplace(String searchParam, String replacedParam, String base) {
