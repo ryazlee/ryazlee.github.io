195e324960009683c6d0ecfaa4a8ba410f135a58
ryazlee <ryan.j.lee99@gmail.com>
2017-12-01 08:14:15 -0800

    changed selscripter to be more user friendser friendly with easier messages

--- projects/mpv/selscripter.py
+++ projects/mpv/selscripter.py
@@ -6,6 +6,7 @@ user = input("Please enter your CalNet id: ");
 pwd = getpass.getpass("Please enter your CalNet password: ");
 
 driver = webdriver.Chrome(executable_path=r"chromedriver")
+
 driver.get("https://auth.berkeley.edu/cas/login?service=https%3A%2F%2Fcalcentral.berkeley.edu%2Fauth%2Fcas%2Fcallback%3Furl%3Dhttps%253A%252F%252Fcalcentral.berkeley.edu%252F/finances")
 
 #driver.get("https://calcentral.berkeley.edu/finances");
@@ -21,16 +22,19 @@ try:
     driver.implicitly_wait(3)
 
     driver.find_element_by_xpath('//*[@id="cc-navigation"]/div/div/ul/li[3]')
-
+    
     html = driver.execute_script("return document.body;");
     data = driver.page_source;
 
-    start = data.find("mealpoints | number") + 21;
-    end = data.find("cc-cal1card-points") - 21;
 
-    #print(data);
-    #print(str(start) + "    "  + str(end));
-    print("You have " + data[start:end] + " meal points");
+    if data.find("mealpoints | number") == -1:
+        print("This was the first time you visited.  The script will work the next time you run");
+    else:    
+        start = data.find("mealpoints | number") + 21; 
+        end = data.find("cc-cal1card-points") - 21;
+        #print(data);
+        #print(str(start) + "    "  + str(end));
+        print("You have " + data[start:end] + " meal points");
 
 except:
     print("Something went wrong with login, please try again");
