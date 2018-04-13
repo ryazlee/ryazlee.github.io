50ea71a1404ffa381c2e157b42e9e40a965cd345
ryazlee <ryan.j.lee99@gmail.com>
2018-04-13 16:41:12 -0700

    fixed js to show the correct tab

--- js/myscript.js
+++ js/myscript.js
@@ -2,16 +2,25 @@ function showprojectsgroup() {
     document.getElementById('projectsgroup').style.display = "block";
     document.getElementById('coursesgroup').style.display = "none";
     document.getElementById('aboutgroup').style.display = "none";
+    document.getElementById('projectsbutton').style.color = "blue";
+    document.getElementById('coursesbutton').style.color = "black";
+    document.getElementById('aboutbutton').style.color = "black";
 }
 
 function showcoursesgroup() {
     document.getElementById('projectsgroup').style.display = "none";
     document.getElementById('coursesgroup').style.display = "block";
     document.getElementById('aboutgroup').style.display = "none";
+    document.getElementById('projectsbutton').style.color = "black";
+    document.getElementById('coursesbutton').style.color = "blue";
+    document.getElementById('aboutbutton').style.color = "black";
 }
 
 function showaboutgroup() {
     document.getElementById('projectsgroup').style.display = "none";
     document.getElementById('coursesgroup').style.display = "none";
     document.getElementById('aboutgroup').style.display = "block";
+    document.getElementById('projectsbutton').style.color = "black";
+    document.getElementById('coursesbutton').style.color = "black";
+    document.getElementById('aboutbutton').style.color = "blue";
 }
