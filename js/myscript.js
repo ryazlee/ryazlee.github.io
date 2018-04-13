7d52a75f0197d702390031d5172da0165a50c085
ryazlee <ryan.j.lee99@gmail.com>
2018-04-13 10:54:43 -0700

    added the about page

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,17 @@
 function showprojectsgroup() {
     document.getElementById('projectsgroup').style.display = "block";
     document.getElementById('coursesgroup').style.display = "none";
+    document.getElementById('aboutgroup').style.display = "none";
 }
 
 function showcoursesgroup() {
     document.getElementById('projectsgroup').style.display = "none";
     document.getElementById('coursesgroup').style.display = "block";
+    document.getElementById('aboutgroup').style.display = "none";
+}
+
+function showaboutgroup() {
+    document.getElementById('projectsgroup').style.display = "none";
+    document.getElementById('coursesgroup').style.display = "none";
+    document.getElementById('aboutgroup').style.display = "block";
 }
