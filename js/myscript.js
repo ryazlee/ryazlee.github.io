61e3ef69d893a2864f6ed4bcb62e1ed04f007a91
ryazlee <ryan.j.lee99@gmail.com>
2018-04-13 09:35:47 -0700

    added the replaceable div for the scrollable window

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,9 @@
-$("#buttons a").click(function() {
-    var id = $(this).attr("id");
-    $("#pages div").css("display", "none");
-    $("#pages div#" + id + "").css("display", "block");
-});
+function showprojectsgroup() {
+    document.getElementById('projectsgroup').style.display = "block";
+    document.getElementById('coursesgroup').style.display = "none";
+}
+
+function showcoursesgroup() {
+    document.getElementById('projectsgroup').style.display = "none";
+    document.getElementById('coursesgroup').style.display = "block";
+}
