2d2c4227c14c62996d6a273e3ad381968c4cd697
Ryan Lee (Non-OVT) <ryan.lee@ovt.com>
2018-07-05 09:54:09 -0700

    Format code

--- js/myscript.js
+++ js/myscript.js
@@ -24,18 +24,18 @@ $(document).ready(function () {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 $('html, body').animate({
-                scrollTop: target.offset().top
+                    scrollTop: target.offset().top
                 }, 500, function() {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
-                    return false;
-                } else {
-                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
-                };
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
                 });
             }
         }
