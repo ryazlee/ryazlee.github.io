fd688d142fefd471ba853ddc48d78ff9fbec48fd
Ryan Lee <ryanjlee@berkeley.edu>
2019-02-02 20:07:36 -0800

    Fix react to be static bc for WHATEVER REASON GITHUB DOES NOT ALLOW YOU TO USE REACT FOR PERSONAL REPO SITES

--- site/src/data/data.js
+++
@@ -1,5 +0,0 @@
-const links_data = [   
-    {"name":"LinkedIn", "source": "https://www.sourceedin.com/in/ryazlee/"},
-    {"name":"Github", "source": "https://github.com/ryazlee"},
-    {"name":"Email", "source": "mailto:ryanjlee@berkeley.edu"}
-];
