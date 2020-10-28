f3c1c0e1ed0c7e64aa1d6188497331420eac822d
Ryan Lee <ryan_j_lee@apple.com>
2020-10-28 10:02:27 -0700

    Add frame and dynamic styling based on screen width

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Main from './components/Main.js'
 import Footer from './components/Footer.js'
+import Frame from './components/Frame.js'
 import './styles.css';
 
 function App() {
   return (
-    <div className="center">
-      <div className="landing">
-        <Main/>
-        <Footer />
+    <div>
+      <Frame />
+      <div className="center">
+        <div className="landing">
+          <Main/>
+          <Footer />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Frame.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles.css'
+import { connectLinks } from '../config.js'
+import peeking_bitmoji from '../assets/peeking_bitmoji.png'
+
+class Frame extends Component {
+    render () {
+      return (
+        <div className="bottomBitmoji">
+           <img src={peeking_bitmoji} alt="Peeking Bitmoji"></img> 
+        </div>
+      );
+    }
+  } 
+  
+  export default Frame;
