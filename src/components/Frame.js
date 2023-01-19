eed9eda0a4d4c23f8d2e31a17e100a92ac5d8d7a
Ryan Lee <rylee@aurora.tech>
2023-01-18 17:22:15 -0800

    Style frame.js

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -3,13 +3,13 @@ import peeking_bitmoji from '../assets/peeking_bitmoji.png'
 import '../styles.scss'
 
 class Frame extends Component {
-    render () {
-      return (
-        <div className="bottomBitmoji">
-           <img src={peeking_bitmoji} alt="Peeking Bitmoji"></img> 
-        </div>
-      );
-    }
-  } 
-  
-  export default Frame;
+  render() {
+    return (
+      <div className="bottomBitmoji">
+        <img src={peeking_bitmoji} alt="Peeking Bitmoji"></img>
+      </div>
+    );
+  }
+}
+
+export default Frame;
